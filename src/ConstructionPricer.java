42252e00e34b729fedf89a16f69d0f8dfafdb574
BT_4N2_29 <Jcreary7087@bths.edu>
2022-09-23 12:34:10 -0400

    y

--- src/ConstructionPricer.java
+++ src/ConstructionPricer.java
@@ -3,16 +3,20 @@ public class ConstructionPricer {
     private final double LUMBER_COST = 11.50;
     private final double WINDOW_COST = 25.75;
 
-    private double taxRate;
-
 
     public ConstructionPricer(double amountOfLumber, double amountOfWindows, double taxInArea) {
 
         double totalCost, tCtax;
         double lumberCosts, windowCosts;
 
+        lumberCosts = amountOfLumber * LUMBER_COST;
+        windowCosts = amountOfWindows * WINDOW_COST;
 
-        System.out.println("Total Cost: ");
+        totalCost = lumberCosts + windowCosts;
+        tCtax = totalCost + (totalCost *taxInArea)
+
+
+        System.out.println("Total Cost: " + totalCost +".\n Total Cost with Tax: " + tCtax);
 
     }
 
