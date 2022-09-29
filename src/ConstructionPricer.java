73618a4ac0f85622e59ef2669ee78571d0fb226d
Antwi <antwinecreary@gmail.com>
2022-09-29 19:30:10 -0400

    Finished.

--- src/ConstructionPricer.java
+++ src/ConstructionPricer.java
@@ -1,23 +1,40 @@
+import java.text.DecimalFormat;
+
 public class ConstructionPricer {
 
     private final double LUMBER_COST = 11.50;
     private final double WINDOW_COST = 25.75;
 
+    private double amountOfLumber, amountOfWindows, taxRate;
+    private double lumberCosts, windowCosts;
+    private double totalCost, totalWithTax;
 
-    public ConstructionPricer(double amountOfLumber, double amountOfWindows, double taxInArea) {
-
-        double totalCost, tCtax;
-        double lumberCosts, windowCosts;
-
-        lumberCosts = amountOfLumber * LUMBER_COST;
-        windowCosts = amountOfWindows * WINDOW_COST;
-
-        totalCost = lumberCosts + windowCosts;
-        tCtax = totalCost + (totalCost *taxInArea)
+    DecimalFormat df = new DecimalFormat("0.00");
 
 
-        System.out.println("Total Cost: " + totalCost +".\n Total Cost with Tax: " + tCtax);
+    public ConstructionPricer(double amountOfLumber, double amountOfWindows, double taxRate) {
 
+        this.amountOfLumber = amountOfLumber;
+        this.amountOfWindows = amountOfWindows;
+        this.taxRate = taxRate;
+
+
+    }
+
+    private void calculateCosts() {
+
+        this.lumberCosts = this.amountOfLumber * LUMBER_COST;
+        this.windowCosts = this.amountOfWindows * WINDOW_COST;
+
+        this.totalCost = this.lumberCosts + this.windowCosts;
+        this.totalWithTax = (this.totalCost * this.taxRate) + this.totalCost;
+
+    }
+
+    public void printCosts() {
+        calculateCosts();
+
+        System.out.println("Total Cost: " + df.format(this.totalCost) + "\nTotal with Tax: " + df.format(this.totalWithTax));
     }
 
 }
